8f21fc9820bed513b51f5233873153d67a5d7ad3
hawkxc068 <hawkxc068@gmail.com>
2025-08-25 08:16:50 +0700

    update: add analytics and speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import ScrollToTopButton from '@/components/ScrollToTopButton';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +22,8 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <ScrollToTopButton />
+        <Analytics /> 
+        <SpeedInsights />
       </body>
     </html>
   );
